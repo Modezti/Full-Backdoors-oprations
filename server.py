396e5a72b9ff4b4adeae5b4338609505564aa2be
Modezti <139481082+Modezti@users.noreply.github.com>
2023-07-13 19:25:17 +0200

    Update server.py

--- server.py
+++ server.py
@@ -9,26 +9,22 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server.bind((ip, port))
 server.listen(1)
-print("server en ecoute...")
+print("server listen...")
 
 client, addresse = server.accept()
 print(f"connection de {addresse}")
 
 def send_message(client_socket, message):
-    # envoyer la longeur du message
     longueur_message = len(message.encode())
     client_socket.sendall(struct.pack("!I", longueur_message))
-    # envoyer a present le message
     client_socket.sendall(message.encode())
 
 def receive_message(client_socket):
-    # recoit d'abord la longueur du message
     get_data_len = client_socket.recv(4)
     message_len = struct.unpack("!I", get_data_len)[0]
     print("length message client is: ", message_len)
 
-    # reception du message proprement dit
-    message = b""
+    # envoyer la longeur du message    message = b""
     while len(message) < message_len:
         chunk = message_len - len(message)
         chunk_size = bufsize if chunk > bufsize else chunk
